ea7e772603a708f689d18305f9ad506078c0bcd0
travismacdonald <x2015khn@stfx.ca>
2019-06-20 08:38:26 -0300

    Added test package

---
+++ KeyFinderTests/com/example/smartdrone/NoteTest.java
@@ -0,0 +1,44 @@
+package com.example.smartdrone;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class NoteTest {
+
+    @Test
+    public void getIx() {
+    }
+
+    @Test
+    public void getRawIx() {
+    }
+
+    @Test
+    public void getName() {
+    }
+
+    @Test
+    public void getNameFlat() {
+    }
+
+    @Test
+    public void getNameSharp() {
+    }
+
+    @Test
+    public void getOctave() {
+    }
+
+    @Test
+    public void getTimer() {
+    }
+
+    @Test
+    public void startNoteTimer() {
+    }
+
+    @Test
+    public void cancelNoteTimer() {
+    }
+}
