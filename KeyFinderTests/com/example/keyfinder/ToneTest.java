fbd5c1bc2021f7de95c1d54a933106aef3855c3a
travismacdonald <x2015khn@stfx.ca>
2019-09-02 14:15:28 -0300

    Passing tests for Tone classes

---
+++ KeyFinderTests/com/example/keyfinder/ToneTest.java
@@ -0,0 +1,38 @@
+package com.example.keyfinder;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ToneTest {
+
+    @Test
+    public void testDegree0() {
+        int testDegree = 0;
+
+        Tone bassTone = new BassTone(testDegree);
+        Tone chordTone = new ChordTone(testDegree);
+
+        assertEquals(testDegree, bassTone.getDegree());
+        assertEquals(testDegree, chordTone.getDegree());
+    }
+
+    @Test
+    public void testDegree15() {
+        int testDegree = 15;
+        Tone bassTone = new BassTone(testDegree);
+        Tone chordTone = new ChordTone(testDegree);
+
+        assertEquals(testDegree, bassTone.getDegree());
+        assertEquals(testDegree, chordTone.getDegree());
+    }
+
+    @Test
+    public void testClassType() {
+        Tone bassTone = new BassTone(0);
+        Tone chordTone = new ChordTone(0);
+
+        assertNotSame(bassTone.getClass(), chordTone.getClass());
+    }
+
+}
