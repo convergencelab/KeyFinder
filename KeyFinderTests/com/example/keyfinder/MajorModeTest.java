6eba7e29cce2db1833a67268b2d87e62efbba1f7
travismacdonald <x2015khn@stfx.ca>
2019-09-02 14:02:26 -0300

    Passing tests for MajorMode.java. Fixed tests for MelodicMinorMode.java

--- KeyFinderTests/com/example/keyfinder/MajorModeTest.java
+++ KeyFinderTests/com/example/keyfinder/MajorModeTest.java
@@ -4,94 +4,94 @@ import org.junit.Test;
 
 import static org.junit.Assert.*;
 
-public class MelodicMinorModeTest {
+public class MajorModeTest {
 
-    // Melodic Minor (Root)
+    // Ionian (Root)
     @Test
     public void testMode0() {
         final int testIx = 0;
-        final int[] testIntervals = {0, 2, 3, 5, 7, 9, 11};
-        final String testName = "Melodic Minor";
+        final int[] testIntervals = {0, 2, 4, 5, 7, 9, 11};
+        final String testName = "Ionian";
 
-        Mode mode = new MelodicMinorMode(testIx);
+        Mode mode = new MajorMode(testIx);
         assertEquals(testIx, mode.getIx());
         assertArrayEquals(testIntervals, mode.getIntervals());
         assertEquals(testName, mode.getName());
     }
 
-    // Phrygian #6
+    // Dorian
     @Test
     public void testMode1() {
         final int testIx = 1;
-        final int[] testIntervals = {0, 1, 3, 5, 7, 9, 10};
-        final String testName = "Phrygian " + MusicTheory.SHARP + "6";
+        final int[] testIntervals = {0, 2, 3, 5, 7, 9, 10};
+        final String testName = "Dorian";
 
-        Mode mode = new MelodicMinorMode(testIx);
+        Mode mode = new MajorMode(testIx);
         assertEquals(testIx, mode.getIx());
         assertArrayEquals(testIntervals, mode.getIntervals());
         assertEquals(testName, mode.getName());
     }
 
-    // Lydian Augmented
+    // Phrygian
     @Test
     public void testMode2() {
         final int testIx = 2;
-        final int[] testIntervals = {0, 2, 4, 6, 8, 9, 11};
-        final String testName = "Lydian Augmented";
+        final int[] testIntervals = {0, 1, 3, 5, 7, 8, 10};
+        final String testName = "Phrygian";
 
-        Mode mode = new MelodicMinorMode(testIx);
+        Mode mode = new MajorMode(testIx);
         assertEquals(testIx, mode.getIx());
         assertArrayEquals(testIntervals, mode.getIntervals());
         assertEquals(testName, mode.getName());
     }
 
-    // Lydian b7
+    // Lydian
     @Test
     public void testMode3() {
         final int testIx = 3;
-        final int[] testIntervals = {0, 2, 4, 6, 7, 9, 10};
-        final String testName = "Lydian " + MusicTheory.FLAT + "7";
+        final int[] testIntervals = {0, 2, 4, 6, 7, 9, 11};
+        final String testName = "Lydian";
 
-        Mode mode = new MelodicMinorMode(testIx);
+        Mode mode = new MajorMode(testIx);
         assertEquals(testIx, mode.getIx());
         assertArrayEquals(testIntervals, mode.getIntervals());
         assertEquals(testName, mode.getName());
     }
 
-    // Mixolydian b6
+    // Mixolydian
     @Test
     public void testMode4() {
         final int testIx = 4;
-        final int[] testIntervals = {0, 2, 4, 5, 7, 8, 10};
-        final String testName = "Mixolydian " + MusicTheory.FLAT + "6";
+        final int[] testIntervals = {0, 2, 4, 5, 7, 9, 10};
+        final String testName = "Mixolydian";
 
-        Mode mode = new MelodicMinorMode(testIx);
+        Mode mode = new MajorMode(testIx);
         assertEquals(testIx, mode.getIx());
         assertArrayEquals(testIntervals, mode.getIntervals());
         assertEquals(testName, mode.getName());
     }
 
-    // Locrian #2
+    // Aeolian
     @Test
     public void testMode5() {
         final int testIx = 5;
-        final int[] testIntervals = {0, 2, 3, 5, 6, 8, 10};
-        final String testName = "Locrian " + MusicTheory.SHARP + "2";
+        final int[] testIntervals = {0, 2, 3, 5, 7, 8, 10};
+        final String testName = "Aeolian";
 
-        Mode mode = new MelodicMinorMode(testIx);
+        Mode mode = new MajorMode(testIx);
         assertEquals(testIx, mode.getIx());
         assertArrayEquals(testIntervals, mode.getIntervals());
         assertEquals(testName, mode.getName());
     }
 
-    // Altered
+    // Locrian
     @Test
     public void testMode6() {
         final int testIx = 6;
-        final int[] testIntervals = {0, 1, 3, 4, 6, 8, 10};
-        final String testName = "Altered";
+        final int[] testIntervals = {0, 1, 3, 5, 6, 8, 10};
+        final String testName = "Locrian";
 
-        Mode mode = new MelodicMinorMode(testIx);
+        Mode mode = new MajorMode(testIx);
         assertEquals(testIx, mode.getIx());
         assertArrayEquals(testIntervals, mode.getIntervals());
         assertEquals(testName, mode.getName());
