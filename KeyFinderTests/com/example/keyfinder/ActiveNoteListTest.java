649dea5ac3a2c2d66ac72a4d450ff16d30ff56af
travismacdonald <x2015khn@stfx.ca>
2019-09-05 21:11:50 -0300

    Added more working test for ActiveNoteList

--- KeyFinderTests/com/example/keyfinder/ActiveNoteListTest.java
+++ KeyFinderTests/com/example/keyfinder/ActiveNoteListTest.java
@@ -30,11 +30,50 @@ public class ActiveNoteListTest {
     }
 
     @Test
+    public void testNumActiveNotes() {
+        /* Could use any notes for testing */
+
+        ActiveNoteList list = makeTestListMajor();
+        assertEquals(0, list.numActiveNotes());
+
+        // Add 'C'
+        list.addNote(0);
+        assertEquals(1, list.numActiveNotes());
+
+        // Add 'C'
+        list.addNote(0);
+        assertEquals(2, list.numActiveNotes());
+
+        // Add 'C#' / 'Db'
+        list.addNote(1);
+        assertEquals(3, list.numActiveNotes());
+
+        // Remove 'C#' / 'Db'
+        list.removeNote(1);
+        assertEquals(2, list.numActiveNotes());
+
+        // Remove 'C'
+        list.removeNote(0);
+        assertEquals(1, list.numActiveNotes());
+
+        // Remove 'C'
+        list.removeNote(0);
+        assertEquals(0, list.numActiveNotes());
+    }
+
+    @Test
     public void testAddNote() {
         ActiveNoteList list = makeTestListMajor();
 
         int[] expectedKeyStrengths = new int[MusicTheory.TOTAL_NOTES];
-        int[] expectedNoteCounts = new int[MusicTheory.TOTAL_NOTES];
+        int[] expectedNoteCounts   = new int[MusicTheory.TOTAL_NOTES];
+
+        // Test note count
+        for (int i = 0; i < expectedNoteCounts.length; i++) {
+            assertEquals(expectedNoteCounts[i], list.getNoteCount(i));
+        }
+
+        // Test key strength
         for(int i = 0; i < expectedKeyStrengths.length; i++) {
             assertEquals(expectedKeyStrengths[i], list.getNoteCount(i));
         }
@@ -42,13 +81,12 @@ public class ActiveNoteListTest {
         // Add C
         list.addNote(0);
         expectedKeyStrengths = new int[]{ 1, 1, 0, 1, 0, 1, 0, 1, 1, 0, 1, 0 };
+        expectedNoteCounts   = new int[]{ 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
 
-        // Test note count
         for (int i = 0; i < expectedNoteCounts.length; i++) {
-//            assertEquals(expectedNoteCounts[i], /* here */);
+            assertEquals(expectedNoteCounts[i], list.getNoteCount(i));
         }
 
-        // Test key strengths
         for(int i = 0; i < expectedKeyStrengths.length; i++) {
             assertEquals(expectedKeyStrengths[i], list.getKeyStrength(i));
         }
@@ -60,7 +98,5 @@ public class ActiveNoteListTest {
         list.setIterateSequence(majorSequence);
         return list;
     }
-
-//    private boolean compareKeyStrength(int[] exp)
 
 }
