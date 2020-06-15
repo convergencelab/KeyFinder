7625cd0ee6e5e8387a92f5fb7525108cb0a87211
travismacdonald <x2015khn@stfx.ca>
2020-06-14 21:06:11 -0300

    Added tests for ActiveNoteList

--- KeyFinderTests/com/example/keyfinder/ActiveNoteListTest.java
+++ KeyFinderTests/com/example/keyfinder/ActiveNoteListTest.java
@@ -1,8 +1,7 @@
 package com.example.keyfinder;
 
+import org.junit.Assert;
 import org.junit.Test;
-
-import static org.junit.Assert.*;
 
 public class ActiveNoteListTest {
 
@@ -11,92 +10,148 @@ public class ActiveNoteListTest {
     private static final int[] melMinSequence = MusicTheory.DORIAN_FLAT2_SEQUENCE;
 
     @Test
-    public void testSize() {
-        ActiveNoteList list = makeTestListMajor();
-
-        assertEquals(0, list.numActiveNotes());
-
-        list.addNote(0);
-        assertEquals(1, list.numActiveNotes());
-
-        list.addNote(0);
-        assertEquals(2, list.numActiveNotes());
-
-        list.removeNote(0);
-        assertEquals(1, list.numActiveNotes());
-
-        list.removeNote(0);
-        assertEquals(0, list.numActiveNotes());
+    public void addNoteC_activeNoteList() {
+        ActiveNoteList anl = new ActiveNoteList();
+        anl.setIterateSequence(majorSequence);
+        anl.addNote(0);
+        boolean[] expected = {
+                true, false, false, false, false, false,
+                false, false, false, false, false, false
+        };
+        Assert.assertArrayEquals(expected, anl.getNoteActiveList());
     }
 
     @Test
-    public void testNumActiveNotes() {
-        /* Could use any notes for testing */
-
-        ActiveNoteList list = makeTestListMajor();
-        assertEquals(0, list.numActiveNotes());
-
-        // Add 'C'
-        list.addNote(0);
-        assertEquals(1, list.numActiveNotes());
-
-        // Add 'C'
-        list.addNote(0);
-        assertEquals(2, list.numActiveNotes());
-
-        // Add 'C#' / 'Db'
-        list.addNote(1);
-        assertEquals(3, list.numActiveNotes());
-
-        // Remove 'C#' / 'Db'
-        list.removeNote(1);
-        assertEquals(2, list.numActiveNotes());
-
-        // Remove 'C'
-        list.removeNote(0);
-        assertEquals(1, list.numActiveNotes());
-
-        // Remove 'C'
-        list.removeNote(0);
-        assertEquals(0, list.numActiveNotes());
+    public void addNoteC_noteWeightList() {
+        ActiveNoteList anl = new ActiveNoteList();
+        anl.setIterateSequence(majorSequence);
+        anl.addNote(0);
+        int[] expected = {
+                1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+        };
+        Assert.assertArrayEquals(expected, anl.getNoteWeightList());
     }
 
     @Test
-    public void testAddNote() {
-        ActiveNoteList list = makeTestListMajor();
-
-        int[] expectedKeyStrengths = new int[MusicTheory.TOTAL_NOTES];
-        int[] expectedNoteCounts   = new int[MusicTheory.TOTAL_NOTES];
-
-        // Test note count
-        for (int i = 0; i < expectedNoteCounts.length; i++) {
-            assertEquals(expectedNoteCounts[i], list.getNoteCount(i));
-        }
-
-        // Test key strength
-        for(int i = 0; i < expectedKeyStrengths.length; i++) {
-            assertEquals(expectedKeyStrengths[i], list.getNoteCount(i));
-        }
-
-        // Add C
-        list.addNote(0);
-        expectedKeyStrengths = new int[]{ 1, 1, 0, 1, 0, 1, 0, 1, 1, 0, 1, 0 };
-        expectedNoteCounts   = new int[]{ 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
-
-        for (int i = 0; i < expectedNoteCounts.length; i++) {
-            assertEquals(expectedNoteCounts[i], list.getNoteCount(i));
-        }
-
-        for(int i = 0; i < expectedKeyStrengths.length; i++) {
-            assertEquals(expectedKeyStrengths[i], list.getKeyStrength(i));
-        }
+    public void addNoteC_keyStrengthList() {
+        ActiveNoteList anl = new ActiveNoteList();
+        anl.setIterateSequence(majorSequence);
+        anl.addNote(0);
+        int[] expected = {
+                1, 1, 0, 1, 0, 1, 0, 1, 1, 0, 1, 0
+        };
+        Assert.assertArrayEquals(expected, anl.getKeyStrengths());
     }
 
-    // Just to encapsulate the setup
-    private ActiveNoteList makeTestListMajor() {
-        ActiveNoteList list = new ActiveNoteList();
-        list.setIterateSequence(majorSequence);
-        return list;
+    @Test
+    public void addNoteC_keyWeightList() {
+        ActiveNoteList anl = new ActiveNoteList();
+        anl.setIterateSequence(majorSequence);
+        anl.addNote(0);
+        int[] expected = {
+                1, 1, 0, 1, 0, 1, 0, 1, 1, 0, 1, 0
+        };
+        Assert.assertArrayEquals(expected, anl.getKeyWeights());
+    }
+
+    @Test
+    public void addNoteC_maxKeyStrength() {
+        ActiveNoteList anl = new ActiveNoteList();
+        anl.setIterateSequence(majorSequence);
+        anl.addNote(0);
+        Assert.assertEquals(anl.getMaxKeyStrength(), 1);
+    }
+
+    @Test
+    public void addNoteC_maxKeyWeight() {
+        ActiveNoteList anl = new ActiveNoteList();
+        anl.setIterateSequence(majorSequence);
+        anl.addNote(0);
+        Assert.assertEquals(anl.getMaxKeyWeight(), 1);
+    }
+
+    @Test
+    public void addNoteCTwice_activeNoteList() {
+        ActiveNoteList anl = new ActiveNoteList();
+        anl.setIterateSequence(majorSequence);
+        anl.addNote(0);
+        anl.addNote(0);
+        boolean[] expected = {
+                true, false, false, false, false, false,
+                false, false, false, false, false, false
+        };
+        Assert.assertArrayEquals(expected, anl.getNoteActiveList());
+    }
+
+    @Test
+    public void addNoteCTwice_noteWeightList() {
+        ActiveNoteList anl = new ActiveNoteList();
+        anl.setIterateSequence(majorSequence);
+        anl.addNote(0);
+        anl.addNote(0);
+        int[] expected = {
+                2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+        };
+        Assert.assertArrayEquals(expected, anl.getNoteWeightList());
+    }
+
+    @Test
+    public void addNoteCTwice_keyStrengthList() {
+        ActiveNoteList anl = new ActiveNoteList();
+        anl.setIterateSequence(majorSequence);
+        anl.addNote(0);
+        anl.addNote(0);
+        int[] expected = {
+                1, 1, 0, 1, 0, 1, 0, 1, 1, 0, 1, 0
+        };
+        Assert.assertArrayEquals(expected, anl.getKeyStrengths());
+    }
+
+    @Test
+    public void addNoteCTwice_keyWeightList() {
+        ActiveNoteList anl = new ActiveNoteList();
+        anl.setIterateSequence(majorSequence);
+        anl.addNote(0);
+        anl.addNote(0);
+        int[] expected = {
+                2, 2, 0, 2, 0, 2, 0, 2, 2, 0, 2, 0
+        };
+        Assert.assertArrayEquals(expected, anl.getKeyWeights());
+    }
+
+    @Test
+    public void addNoteCTwice_maxKeyStrength() {
+        ActiveNoteList anl = new ActiveNoteList();
+        anl.setIterateSequence(majorSequence);
+        anl.addNote(0);
+        anl.addNote(0);
+        Assert.assertEquals(anl.getMaxKeyStrength(), 1);
+    }
+
+    @Test
+    public void addNoteCTwice_maxKeyWeight() {
+        ActiveNoteList anl = new ActiveNoteList();
+        anl.setIterateSequence(majorSequence);
+        anl.addNote(0);
+        anl.addNote(0);
+        Assert.assertEquals(anl.getMaxKeyWeight(), 2);
+    }
+
+    @Test
+    public void addMultipleNotesThenRemove() {
+        ActiveNoteList anl = new ActiveNoteList();
+        anl.setIterateSequence(majorSequence);
+        anl.addNote(0);
+        anl.addNote(0);
+        anl.addNote(0);
+        anl.addNote(0);
+        anl.addNote(0);
+        anl.addNote(0);
+        anl.removeNote(0);
+        int[] expected = {
+                0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+        };
+        Assert.assertArrayEquals(expected, anl.getKeyWeights());
     }
 
 }
